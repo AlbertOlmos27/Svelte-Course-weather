4e31f4eef8765efbfc4ef9a992ab5fdca37f5e05
Albert <albertolmos346@gmail.com>
2023-05-06 15:00:31 -0500

    fixed page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,20 @@
 <script>
 import { getWeatherFrom } from '../services/weather';
 import WeatherFooter from '../components/weatherFooter.svelte';
-import WeatherIcon from '../components/weather-icons.svelte';
+import WeatherIcons from '../components/weather-icons.svelte';
 
 const weatherPromise = getWeatherFrom();
+console.log('weatherPromise :>> ', weatherPromise);
 </script>
 
 {#await weatherPromise then weather}
   <section>
-    <h1>{weather.locationName}</h1>
-    <h2>{weather.temperature}°</h2>
-    <h3>☁{weather.conditionText}</h3>
-    <WeatherIcon text={weather.conditionText} icon={weather.conditionIcon}/>
+    <h1><b>{weather.body.locationName}</b></h1>
+    <h1>{weather.body.country}</h1>
+    <h2>{weather.body.temperature}°</h2>
+    <h3>☁{weather.body.conditionText}</h3>
+    <h4><strong>Hour:</strong> {weather.body.localtime}</h4>
+    <WeatherIcons text={weather.body.conditionText} icon={weather.body.conditionIcon}/>
   </section>
   <WeatherFooter/>
 {/await}
@@ -20,17 +23,16 @@ const weatherPromise = getWeatherFrom();
   section {
     padding: 1.6rem;
   }
-  h1 {
+  h1, h2, h4 {
     font-weight: 300;
     color: #333;
     text-transform: uppercase;
+  }
+  h1 {
     padding: 1.6rem 0 0 0;
   }
   h2 {
-    font-weight: 300;
     font-size: 12rem;
-    color: #333;
-    text-transform: uppercase;
     padding: 0;
   }
   h3 {
